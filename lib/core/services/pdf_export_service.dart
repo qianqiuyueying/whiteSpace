3dd6b8e22a77cf1c31fdd2f8361e4be41edee151
qianqiuyueying <3044481323@qq.com>
2026-03-08 20:47:09 +0800

    chore: remove unused import in pdf_export_service.dart

--- lib/core/services/pdf_export_service.dart
+++ lib/core/services/pdf_export_service.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 // import 'package:printing/printing.dart';  // 暂时禁用
